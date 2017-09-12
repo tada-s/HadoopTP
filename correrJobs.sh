126c28cd65a8359d9509a86291b79923a64d35ed
Tadashi <akagi.tada@gmail.com>
2017-09-12 10:05:37 -0400

    Update correrJobs.sh

--- correrJobs.sh
+++ correrJobs.sh
@@ -2,6 +2,6 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir /user/hduser/inputTP/
 /usr/local/hadoop/bin/hdfs dfs -put ./input.txt /user/hduser/inputTP
 /usr/local/hadoop/bin/hdfs dfs -rm -R /user/hduser/outputTP
-/usr/local/hadoop/bin/hadoop jar ./TPBigData.jar calcVisitTime.CalcVisitTimeJob /user/hduser/inputTP/input.txt /user/hduser/outputTP/output1.txt
-/usr/local/hadoop/bin/hadoop jar ./TPBigData.jar promedio.PromedioJob /user/hduser/outputTP/output1.txt /user/hduser/outputTP/output2.txt
-/usr/local/hadoop/bin/hadoop jar ./TPBigData.jar ranking.RankingJob /user/hduser/outputTP/output2.txt /user/hduser/outputTP/output3.txt
+/usr/local/hadoop/bin/hadoop jar ./TPBigData.jar calcVisitTime.CalcVisitTimeJob /user/hduser/inputTP/input.txt /user/hduser/outputTP/output1
+/usr/local/hadoop/bin/hadoop jar ./TPBigData.jar promedio.PromedioJob /user/hduser/outputTP/output1 /user/hduser/outputTP/output2
+/usr/local/hadoop/bin/hadoop jar ./TPBigData.jar ranking.RankingJob /user/hduser/outputTP/output2 /user/hduser/outputTP/output3
